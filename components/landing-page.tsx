a9c4c766f152a3252d1c84690cf1a23061118e49
gr-trading15 <110227948+gr-trading15@users.noreply.github.com>
2025-09-06 18:28:49 +0530

    ideogram api

--- components/landing-page.tsx
+++ components/landing-page.tsx
@@ -238,6 +238,7 @@ export function LandingPage() {
         }
         const fileName =
           doc.getElementsByTagName("fileName")[0]?.textContent?.trim() ?? null;
+        setIsUploading(false);
         window.location.href =
           selectedProduct?.handle +
           `&uploadFileName=${fileName}&uploadRemoveBackground=true`;
